d1851b369b006a76ff5772d2ca0fd4d8a0fabf89
Srikee <kee19384@gmail.com>
2020-08-22 21:33:05 +0700

    init project

---
+++ example.php
@@ -0,0 +1,15 @@
+<?php
+    echo json_encode(array(
+        "status"=>true,
+        "data"=>array(
+            array(
+                "id"=>"1",
+                "name"=>"รีสอร์ทบ้านไม่ริมเล"
+            ),
+            array(
+                "id"=>"2",
+                "name"=>"ดาดฟ้ารีสอร์ท"
+            ),
+        )
+    ));
+    
