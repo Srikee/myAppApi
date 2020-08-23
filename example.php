3f68e003581b5cfd4f970aa97473897a52462071
Srikee <kee19384@gmail.com>
2020-08-23 10:23:29 +0700

    Add config

--- example.php
+++ example.php
@@ -1,6 +1,13 @@
 <?php
+    include("config/autoload.php");
+
+    $val1 = @$REQUEST->val1;
+    $val2 = @$REQUEST->val2;
+
     echo json_encode(array(
         "status"=>true,
+        "val1"=>$val1,
+        "val2"=>$val2,
         "data"=>array(
             array(
                 "id"=>"1",
@@ -10,6 +17,6 @@
                 "id"=>"2",
                 "name"=>"ดาดฟ้ารีสอร์ท"
             ),
-        )
+        ),
     ));
     
